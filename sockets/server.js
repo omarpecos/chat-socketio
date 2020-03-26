c5d0748bc49ccc15890f975a06154408a048db41
omarpecos <omarpecos@gmail.com>
2020-03-26 17:57:53 +0100

    Mejoras v2

--- sockets/server.js
+++ sockets/server.js
@@ -22,7 +22,9 @@ io.on('connect',function(socket){
     console.log('nueva conexion id: '+socket.id );
     
     socket.on('datos_usuario',function(datos){
+
         usuario = datos.usuario;
+        console.log("datos usaurio >> " + datos.usuario);
         id_socket = socket.id; 
 
         console.log("--------------------------------------------");
@@ -35,19 +37,21 @@ io.on('connect',function(socket){
            IdsOnUsers[usuario] = new Array();
         }
         IdsOnUsers[usuario].push(id_socket);
-        console.log("USUARIOS ONLINE")
+       /* console.log("USUARIOS ONLINE")
         console.log('>>>>> Usuarios por id ---->>> ');
         console.log(UsersOnId);
         console.log('>>>>> Ids por Users ---->>> ');
-        console.log(IdsOnUsers);
+        console.log(IdsOnUsers);*/
         console.log("--------------- Usuarios conectados ---->  "+ Object.keys(IdsOnUsers).length);
         
        
-        io.emit('nuevo_usuario',{user : usuario});
+        io.emit('nuevo_usuario',{usuario : usuario});
         io.emit('users',{users : Object.keys(IdsOnUsers)});
     });
 
     socket.on('send_mensaje',function(datos){
+
+        console.log(JSON.stringify(datos));
 
         if (datos.destinatario != null){
             console.log(datos.usuario+' esta eviando un mensaje privado');
@@ -57,13 +61,13 @@ io.on('connect',function(socket){
                 // console.log("destino >>> "+destinatario+" Y sus dirs >>> "+dirs);
 
             for (var i=0;i<dirs.length;i++){
-                io.to(dirs[i]).emit('nuevo_mensaje',{user : datos.usuario, mensaje : datos.mensaje, destino: destinatario});
+                io.to(dirs[i]).emit('nuevo_mensaje',{usuario : datos.usuario, mensaje : datos.mensaje, destino: destinatario});
             }
-            io.to(socket.id).emit('nuevo_mensaje',{user : datos.usuario, mensaje : datos.mensaje,});
+            io.to(socket.id).emit('nuevo_mensaje',{usuario : datos.usuario, mensaje : datos.mensaje,});
            
         }else{
             console.log(datos.usuario+' esta eviando un mensaje NORMAL');
-            io.emit('nuevo_mensaje',{user : datos.usuario, mensaje : datos.mensaje , destino: 'todos'});
+            io.emit('nuevo_mensaje',{usuario : datos.usuario, mensaje : datos.mensaje , destino: 'todos'});
         }
         
     });
